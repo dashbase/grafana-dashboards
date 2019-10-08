7677a6c4681581335e01464ae46e5922e84805b6
yangzb09 <zhibin@dashbase.io>
2019-10-08 18:28:39 +0800

    Update update.sh

--- .circleci/update.sh
+++ .circleci/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ${CIRCLE_BRANCH} == "master" ]; then
-git clone -b Add_grafana_build git@github.com:dashbase/dashbase.git
+git clone git@github.com:dashbase/dashbase.git
 cd dashbase
 sed -i 's/grafana-dashboards\ tag:.*/#grafana-dashboards\ tag:${CIRCLE_SHA1::6}/g' ./docker/grafana/build.sh
 git config user.name "CI"
